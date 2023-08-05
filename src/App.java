c4d8c67773f47232f271e7b3ae890e52ad6d12be
Lavínia Winter <lavinia_000@outlook.com>
2023-08-05 00:09:21 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -7,16 +7,58 @@ public class App {
 
 
     public static void main(String[] args) {
-        HashMap<String, String> hashMap = new HashMap<>();
+
+        //StringBuilder concatenated = new StringBuilder();
+        HashMap<String, Character> hashMap = new HashMap<>();
         LinkedList<Character> linkedList = new LinkedList<>();
 
-        try{
-            FileReader reader = new FileReader("testes\\caso0010.txt");
-            int element;
+        hashMap.put("AD", 'N');
+        hashMap.put("AN", 'D');
+        hashMap.put("DA", 'N');
+        hashMap.put("DN", 'A');
+        hashMap.put("NA", 'D');
+        hashMap.put("ND", 'A');
 
-            while ((element = reader.read()) != -1) {
-                char character = (char) element;
-                linkedList.add(character);
+
+
+
+        try{
+            FileReader reader = new FileReader("testes\\testecurto.txt");
+            int element1;
+
+            while ((element1 = reader.read()) != -1) {
+                char character1 = (char) element1;
+                int element2 = reader.read();
+
+                String a;
+                if(element2 != -1){
+                    char character2 = (char) element2;
+                    //concatenated.append(character1).append(character2);
+                    a = Character.toString(character1)+ Character.toString(character2);
+                    System.out.println(a.toString());
+                }
+                else
+                {
+                    a = Character.toString(character1);
+                    //concatenated.append(character1);
+                }
+
+            
+                //String str = concatenated.toString();
+                //System.out.println(str);
+                if(hashMap.containsKey(a)){
+                    char value = hashMap.get(a);
+                    System.out.println(value);
+                    linkedList.add(value);
+                    System.out.println(linkedList.toString());
+                }
+                else
+                {
+                    for(char c : a.toCharArray()){
+                        linkedList.add(c);
+                    }
+                }
+                //linkedList.add(character);
                 
             }
             reader.close();
@@ -24,8 +66,8 @@ public class App {
         catch(IOException e){
             e.printStackTrace();
         }
-        for (Character character : linkedList) {
+        /*for (Character character : linkedList) {
             System.out.print(character);
-        }
+        }*/
     }
 }
