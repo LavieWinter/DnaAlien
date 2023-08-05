81b9facfe677e00ab6a7dd84363e790870379467
Lavínia Winter <lavinia_000@outlook.com>
2023-08-05 01:22:45 -0300

    a

--- src/App.java
+++ src/App.java
@@ -3,10 +3,12 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.HashMap;
 
-public class App {
+public class App 
+{
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         HashMap<String, Character> hashMap = new HashMap<>();
         LinkedList<Character> linkedList = new LinkedList<>();
@@ -19,17 +21,20 @@ public class App {
         hashMap.put("ND", 'A');
 
 
-        try{
+        try
+        {
             FileReader reader = new FileReader("testes\\caso0010.txt");
             int element1;
             int count=0;
 
-            while ((element1 = reader.read()) != -1) {
+            while ((element1 = reader.read()) != -1) 
+            {
                 char character1 = (char) element1;
                 int element2 = reader.read();
 
                 String a;
-                if(element2 != -1){
+                if(element2 != -1)
+                {
                     char character2 = (char) element2;
                     a = Character.toString(character1)+ Character.toString(character2);
                 }
@@ -38,7 +43,8 @@ public class App {
                     a = Character.toString(character1);
                 }
 
-                if(hashMap.containsKey(a)){
+                if(hashMap.containsKey(a))
+                {
                     char value = hashMap.get(a);
                     linkedList.add(value);
                     count++;
@@ -46,22 +52,26 @@ public class App {
                 }
                 else
                 {
-                    for(char c : a.toCharArray()){
+                    for(char c : a.toCharArray())
+                    {
                         linkedList.add(c);
                         count++;
                     }
                 }
-                
             }
             reader.close();
             System.out.println(count);
         }
-        catch(IOException e){
+        catch(IOException e)
+        {
             e.printStackTrace();
         }
-        for (Character character : linkedList) {
+
+        //mostra quantos há na fila
+        /*for (Character character : linkedList) 
+        {
             System.out.print(character);
-        }
+        }*/
 
     }
 }
